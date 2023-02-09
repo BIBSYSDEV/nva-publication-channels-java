da16d17f6717737bdeeda8fa61bcfeed187273f3
brinxmat <789709+brinxmat@users.noreply.github.com>
2023-02-09 18:08:13 +0100

    Checked exception -> unchecked

--- src/main/java/no/sikt/nva/pubchannels/handler/FetchJournalRequest.java
+++ src/main/java/no/sikt/nva/pubchannels/handler/FetchJournalRequest.java
@@ -1,7 +1,6 @@
 package no.sikt.nva.pubchannels.handler;
 
 import no.sikt.nva.pubchannels.handler.no.sikt.nva.pubchannels.handler.request.validator.FetchRequestValidator;
-import no.sikt.nva.pubchannels.handler.no.sikt.nva.pubchannels.handler.request.validator.ValidationException;
 import no.sikt.nva.pubchannels.handler.no.sikt.nva.pubchannels.handler.request.validator.Validator;
 import nva.commons.apigateway.RequestInfo;
 
@@ -12,13 +11,13 @@ public final class FetchJournalRequest {
     private final String identifier;
     private final String year;
 
-    public FetchJournalRequest(RequestInfo requestInfo) throws ValidationException {
+    public FetchJournalRequest(RequestInfo requestInfo) {
         this.identifier = requestInfo.getPathParameter(IDENTIFIER_PATH_PARAM_NAME).trim();
         this.year = requestInfo.getPathParameter(YEAR_PATH_PARAM_NAME).trim();
         validate(new FetchRequestValidator());
     }
 
-    private void validate(Validator validator) throws ValidationException {
+    private void validate(Validator validator) {
         validator.validate(this);
     }
 
