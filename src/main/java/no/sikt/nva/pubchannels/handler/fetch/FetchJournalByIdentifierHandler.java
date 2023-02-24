7ce3f49a6488729894e570be6ef172b69aff9711
monaullah <monaullah98@gmail.com>
2023-02-24 13:56:29 +0100

    Build failed. Removed unused import.

--- src/main/java/no/sikt/nva/pubchannels/handler/fetch/FetchJournalByIdentifierHandler.java
+++ src/main/java/no/sikt/nva/pubchannels/handler/fetch/FetchJournalByIdentifierHandler.java
@@ -15,7 +15,6 @@ import java.net.HttpURLConnection;
 import java.net.URI;
 
 import static no.sikt.nva.pubchannels.handler.validator.Validator.validateUuid;
-import static no.sikt.nva.pubchannels.handler.validator.Validator.validateYear;
 import static nva.commons.core.attempt.Try.attempt;
 import static nva.commons.core.paths.UriWrapper.HTTPS;
 
