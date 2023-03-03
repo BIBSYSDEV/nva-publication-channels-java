0cffecf903c58fe784f107beda43f1509969712f
Haakon Heiberg <haakon.heiberg@soprasteria.com>
2023-03-03 08:18:28 +0100

    Fix so that req.homepage = clientreq.url

--- src/main/java/no/sikt/nva/pubchannels/dataporten/model/DataportenCreateJournalRequest.java
+++ src/main/java/no/sikt/nva/pubchannels/dataporten/model/DataportenCreateJournalRequest.java
@@ -7,26 +7,26 @@ public class DataportenCreateJournalRequest {
     private static final String PISSN_FIELD = "pissn";
     private static final String NAME_FIELD = "name";
     private static final String EISSN_FIELD = "eissn";
-    private static final String HOMEPAGE_FIELD = "homepage";
+    private static final String URL_FIELD = "url";
     @JsonProperty(NAME_FIELD)
     private final String name;
     @JsonProperty(PISSN_FIELD)
     private final String printIssn;
     @JsonProperty(EISSN_FIELD)
     private final String onlineIssn;
-    @JsonProperty(HOMEPAGE_FIELD)
-    private final String homepage;
+    @JsonProperty(URL_FIELD)
+    private final String url;
 
     @JsonCreator
     public DataportenCreateJournalRequest(
             @JsonProperty String name,
             @JsonProperty(PISSN_FIELD) String printIssn,
             @JsonProperty(EISSN_FIELD) String onlineIssn,
-            @JsonProperty(HOMEPAGE_FIELD) String homepage) {
+            @JsonProperty(URL_FIELD) String url) {
         this.name = name;
         this.printIssn = printIssn;
         this.onlineIssn = onlineIssn;
-        this.homepage = homepage;
+        this.url = url;
     }
 
     public String getName() {
@@ -41,7 +41,7 @@ public class DataportenCreateJournalRequest {
         return onlineIssn;
     }
 
-    public String getHomepage() {
-        return homepage;
+    public String getUrl() {
+        return url;
     }
 }
