0074c4251be4bbad40bf864bee6f1dddcbdbee4b
monaullah <monaullah98@gmail.com>
2023-03-31 10:10:00 +0200

    Fetch series by identifier and year handler: input validation

---
+++ src/test/java/no/sikt/nva/pubchannels/handler/fetch/series/FetchSeriesByIdentifierAndYearHandlerTest.java
@@ -0,0 +1,123 @@
+package no.sikt.nva.pubchannels.handler.fetch.series;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import no.sikt.nva.pubchannels.dataporten.DataportenPublicationChannelClient;
+import no.unit.nva.stubs.FakeContext;
+import no.unit.nva.stubs.WiremockHttpClient;
+import no.unit.nva.testutils.HandlerRequestBuilder;
+import nva.commons.apigateway.GatewayResponse;
+import nva.commons.core.Environment;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mockito;
+import org.zalando.problem.Problem;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+import static no.unit.nva.commons.json.JsonUtils.dtoObjectMapper;
+import static no.unit.nva.testutils.RandomDataGenerator.randomInteger;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.Mockito.when;
+
+@WireMockTest(httpsEnabled = true)
+class FetchSeriesByIdentifierAndYearHandlerTest {
+    private static final int YEAR_START = 1900;
+    private FetchSeriesByIdentifierAndYearHandler handlerUnderTest;
+    private ByteArrayOutputStream output;
+
+    private static final Context context = new FakeContext();
+    private Environment environment;
+
+    @BeforeEach
+    void setup(WireMockRuntimeInfo runtimeInfo) {
+        environment = Mockito.mock(Environment.class);
+        when(environment.readEnv("ALLOWED_ORIGIN")).thenReturn("*");
+        when(environment.readEnv("API_DOMAIN")).thenReturn("localhost");
+        when(environment.readEnv("CUSTOM_DOMAIN_BASE_PATH")).thenReturn("publication-channels");
+
+        var dataportenBaseUri = URI.create(runtimeInfo.getHttpsBaseUrl());
+        var httpClient = WiremockHttpClient.create();
+        var publicationChannelClient = new DataportenPublicationChannelClient(httpClient, dataportenBaseUri, null);
+        this.handlerUnderTest = new FetchSeriesByIdentifierAndYearHandler(environment, publicationChannelClient);
+        this.output = new ByteArrayOutputStream();
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        output.flush();
+    }
+
+    @ParameterizedTest(name = "year {0} is invalid")
+    @MethodSource("invalidYearsProvider")
+    void shouldReturnBadRequestWhenPathParameterYearIsNotValid(String year)
+            throws IOException {
+
+        var input = constructRequest(year, UUID.randomUUID().toString());
+
+        handlerUnderTest.handleRequest(input, output, context);
+
+        var response = GatewayResponse.fromOutputStream(output, Problem.class);
+
+        assertThat(response.getStatusCode(), is(equalTo(HttpURLConnection.HTTP_BAD_REQUEST)));
+
+        var problem = response.getBodyObject(Problem.class);
+
+        assertThat(problem.getDetail(),
+                is(containsString("Year")));
+    }
+
+    @ParameterizedTest(name = "identifier \"{0}\" is invalid")
+    @ValueSource(strings = {" ", "abcd", "ab78ab78ab78ab78ab78a7ba87b8a7ba87b8"})
+    void shouldReturnBadRequestWhenPathParameterIdentifierIsNotValid(String identifier)
+            throws IOException {
+
+        var input = constructRequest(randomYear(), identifier);
+
+        handlerUnderTest.handleRequest(input, output, context);
+
+        var response = GatewayResponse.fromOutputStream(output, Problem.class);
+
+        assertThat(response.getStatusCode(), is(equalTo(HttpURLConnection.HTTP_BAD_REQUEST)));
+
+        var problem = response.getBodyObject(Problem.class);
+
+        assertThat(problem.getDetail(),
+                is(containsString("Pid")));
+    }
+
+    private static InputStream constructRequest(String year, String identifier) throws JsonProcessingException {
+        return new HandlerRequestBuilder<Void>(dtoObjectMapper)
+                .withPathParameters(Map.of(
+                        "identifier", identifier,
+                        "year", year
+                ))
+                .build();
+    }
+
+    private String randomYear() {
+        var bound = (LocalDate.now().getYear() + 1) - YEAR_START;
+        return Integer.toString(YEAR_START + randomInteger(bound));
+    }
+
+    private static Stream<String> invalidYearsProvider() {
+        String yearAfterNextYear = Integer.toString(LocalDate.now().getYear() + 2);
+        return Stream.of(" ", "abcd", yearAfterNextYear, "21000");
+    }
+}
