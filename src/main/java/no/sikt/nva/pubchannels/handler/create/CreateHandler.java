f7c7227204c05ea927e0ab718663752d3700c405
Haakon Heiberg <haakon.heiberg@soprasteria.com>
2023-04-17 14:21:44 +0200

    Use SecretsManager Api to get secret

--- src/main/java/no/sikt/nva/pubchannels/handler/create/CreateHandler.java
+++ src/main/java/no/sikt/nva/pubchannels/handler/create/CreateHandler.java
@@ -10,6 +10,7 @@ import nva.commons.apigateway.exceptions.UnauthorizedException;
 import nva.commons.core.Environment;
 import nva.commons.core.JacocoGenerated;
 import nva.commons.core.paths.UriWrapper;
+import nva.commons.secrets.SecretsReader;
 
 import java.net.HttpURLConnection;
 import java.net.URI;
@@ -18,21 +19,20 @@ import java.net.http.HttpClient;
 import static nva.commons.core.paths.UriWrapper.HTTPS;
 
 public abstract class CreateHandler<I, O> extends ApiGatewayHandler<I, O> {
-    private static final String ENV_DATAPORTEN_AUTH_BASE_URI = "DATAPORTEN_AUTH_BASE_URL";
     private static final String ENV_DATAPORTEN_PUBLICATION_CHANNEL_BASE_URI =
             "DATAPORTEN_CHANNEL_REGISTRY_BASE_URL";
     private static final String ENV_API_DOMAIN = "API_DOMAIN";
     private static final String ENV_CUSTOM_DOMAIN_BASE_PATH = "CUSTOM_DOMAIN_BASE_PATH";
-    private static final String ENV_DATAPORTEN_AUTH_CLIENT_ID = "DATAPORTEN_AUTH_CLIENT_ID";
-    private static final String ENV_DATAPORTEN_AUTH_CLIENT_SECRET = "DATAPORTEN_AUTH_CLIENT_SECRET";
+    private static final String SECRET_NAME = "DataportenChannelRegistryClientCredentials";
     protected PublicationChannelClient publicationChannelClient;
 
     @JacocoGenerated
     protected CreateHandler(Class<I> iclass, Environment environment) {
         super(iclass, environment);
-        var clientId = environment.readEnv(ENV_DATAPORTEN_AUTH_CLIENT_ID);
-        var clientSecret = environment.readEnv(ENV_DATAPORTEN_AUTH_CLIENT_SECRET);
-        var authBaseUri = URI.create(environment.readEnv(ENV_DATAPORTEN_AUTH_BASE_URI));
+        var secretsReader = new SecretsReader();
+        var clientId = secretsReader.fetchSecret(SECRET_NAME, "clientId");
+        var clientSecret = secretsReader.fetchSecret(SECRET_NAME, "clientSecret");
+        var authBaseUri = URI.create(secretsReader.fetchSecret(SECRET_NAME, "authBaseUrl"));
         var pubChannelBaseUri = URI.create(environment.readEnv(ENV_DATAPORTEN_PUBLICATION_CHANNEL_BASE_URI));
         var authClient = new DataportenAuthClient(HttpClient.newBuilder().build(), authBaseUri, clientId, clientSecret);
         this.publicationChannelClient =
