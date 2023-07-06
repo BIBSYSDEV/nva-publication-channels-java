c31fb88ea6ed06c2e25258261c576b098611b645
Mona Ullah <54892125+monaullah@users.noreply.github.com>
2023-07-06 12:15:35 +0200

    Add grant type as query params (#34)

--- src/main/java/no/sikt/nva/pubchannels/HttpHeaders.java
+++ src/main/java/no/sikt/nva/pubchannels/HttpHeaders.java
@@ -4,7 +4,7 @@ public final class HttpHeaders {
 
     public static final String CONTENT_TYPE = "Content-Type";
     public static final String AUTHORIZATION = "Authorization";
-    public static final String CONTENT_TYPE_X_WWW_FORM_URLENCODED = "x-www-form-urlencoded";
+    public static final String CONTENT_TYPE_X_WWW_FORM_URLENCODED = "application/x-www-form-urlencoded";
     public static final String CONTENT_TYPE_APPLICATION_JSON = "application/json";
     public static final String ACCEPT = "Accept";
     public static final String LOCATION = "Location";
--- src/main/java/no/sikt/nva/pubchannels/dataporten/DataportenAuthClient.java
+++ src/main/java/no/sikt/nva/pubchannels/dataporten/DataportenAuthClient.java
@@ -1,5 +1,15 @@
 package no.sikt.nva.pubchannels.dataporten;
 
+import static no.unit.nva.commons.json.JsonUtils.dtoObjectMapper;
+import static nva.commons.core.attempt.Try.attempt;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Base64;
+import java.util.Map;
 import no.sikt.nva.pubchannels.HttpHeaders;
 import no.sikt.nva.pubchannels.dataporten.model.TokenBodyResponse;
 import no.sikt.nva.pubchannels.handler.AuthClient;
@@ -9,20 +19,10 @@ import nva.commons.core.paths.UriWrapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-
-import static no.unit.nva.commons.json.JsonUtils.dtoObjectMapper;
-import static nva.commons.core.attempt.Try.attempt;
-
 public class DataportenAuthClient implements AuthClient {
+
     private static final Logger LOGGER = LoggerFactory.getLogger(DataportenAuthClient.class);
+    private static final Map<String, String> GRANT_TYPE_CLIENT_CREDENTIALS = Map.of("grant_type", "client_credentials");
     private final HttpClient httpClient;
     private final URI baseUri;
     private final String clientId;
@@ -35,7 +35,6 @@ public class DataportenAuthClient implements AuthClient {
         this.baseUri = baseUri;
     }
 
-
     @Override
     public String getToken() throws ApiGatewayException {
         var request = createTokenRequest(clientId, clientSecret);
@@ -43,13 +42,25 @@ public class DataportenAuthClient implements AuthClient {
         return tokenBody.getAccessToken();
     }
 
+    private static HttpRequest.BodyPublisher clientCredentialsAuthType() {
+        var queryParameters = UriWrapper.fromHost("notimportant")
+                                  .addQueryParameters(GRANT_TYPE_CLIENT_CREDENTIALS).getUri().getRawQuery();
+        return HttpRequest.BodyPublishers.ofString(queryParameters);
+    }
+
+    private static void reportFailingRequest(HttpRequest request, HttpResponse<String> response)
+        throws BadGatewayException {
+        LOGGER.error("Error executing request: {} {} {}", request.uri(), response.statusCode(), response.body());
+        throw new BadGatewayException("Unexpected response from upstream!");
+    }
+
     private HttpRequest createTokenRequest(String clientId, String clientSecret) {
         return HttpRequest.newBuilder()
-                .header(HttpHeaders.CONTENT_TYPE, HttpHeaders.CONTENT_TYPE_X_WWW_FORM_URLENCODED)
-                .header(HttpHeaders.AUTHORIZATION, getCredentialsString(clientId, clientSecret))
-                .uri(getTokenRequestUri())
-                .POST(HttpRequest.BodyPublishers.ofString("grant_type=client_credentials", StandardCharsets.UTF_8))
-                .build();
+                   .header(HttpHeaders.CONTENT_TYPE, HttpHeaders.CONTENT_TYPE_X_WWW_FORM_URLENCODED)
+                   .header(HttpHeaders.AUTHORIZATION, getCredentialsString(clientId, clientSecret))
+                   .uri(getTokenRequestUri())
+                   .POST(clientCredentialsAuthType())
+                   .build();
     }
 
     private String getCredentialsString(String clientId, String clientSecret) {
@@ -60,29 +71,23 @@ public class DataportenAuthClient implements AuthClient {
 
     private URI getTokenRequestUri() {
         return UriWrapper.fromUri(baseUri)
-                .addChild("oauth", "token")
-                .getUri();
+                   .addChild("oauth", "token")
+                   .getUri();
     }
 
     private TokenBodyResponse fetchToken(HttpRequest request) throws ApiGatewayException {
         return attempt(() -> executeRequest(request))
-                .orElseThrow(failure -> logAndCreateBadGatewayException(request.uri(), failure.getException()));
+                   .orElseThrow(failure -> logAndCreateBadGatewayException(request.uri(), failure.getException()));
     }
 
     private TokenBodyResponse executeRequest(HttpRequest request)
-            throws IOException, InterruptedException, BadGatewayException {
+        throws IOException, InterruptedException, BadGatewayException {
         var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
         if (response.statusCode() != HttpURLConnection.HTTP_OK) {
             reportFailingRequest(request, response);
         }
         return attempt(() -> dtoObjectMapper.readValue(response.body(), TokenBodyResponse.class)).orElseThrow();
-    }
-
-    private static void reportFailingRequest(HttpRequest request, HttpResponse<String> response)
-            throws BadGatewayException {
-        LOGGER.error("Error executing request: {} {} {}", request.uri(), response.statusCode(), response.body());
-        throw new BadGatewayException("Unexpected response from upstream!");
     }
 
     private ApiGatewayException logAndCreateBadGatewayException(URI uri, Exception e) {
@@ -95,5 +100,4 @@ public class DataportenAuthClient implements AuthClient {
         }
         return new BadGatewayException("Unable to reach upstream!");
     }
-
 }
