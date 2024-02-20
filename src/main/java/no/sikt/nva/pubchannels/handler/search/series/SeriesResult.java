1fce2986c95a1ef389ae9b22fffe7532d17a2bff
Mona Ullah <54892125+monaullah@users.noreply.github.com>
2024-02-20 08:12:56 +0100

    NP-46246 Add identifier to publication channel response (#49)
    * Add identifier to all fetch and search dtos
    
    * Implement JsonSerializable and toJsonString for toString

--- src/main/java/no/sikt/nva/pubchannels/handler/search/series/SeriesResult.java
+++ src/main/java/no/sikt/nva/pubchannels/handler/search/series/SeriesResult.java
@@ -8,14 +8,16 @@ import java.util.Optional;
 import no.sikt.nva.pubchannels.handler.ScientificValue;
 import no.sikt.nva.pubchannels.handler.ThirdPartySeries;
 import no.sikt.nva.pubchannels.model.Contexts;
+import no.unit.nva.commons.json.JsonSerializable;
 import nva.commons.core.JacocoGenerated;
 import nva.commons.core.paths.UriWrapper;
 
-public final class SeriesResult {
+public final class SeriesResult implements JsonSerializable {
 
     private static final String TYPE_FIELD = "type";
     private static final String CONTEXT_FIELD = "@context";
     private static final String ID_FIELD = "id";
+    private static final String IDENTIFIER_FIELD = "identifier";
     private static final String NAME_FIELD = "name";
     private static final String ONLINE_ISSN_FIELD = "onlineIssn";
     private static final String PRINT_ISSN_FIELD = "printIssn";
@@ -28,6 +30,10 @@ public final class SeriesResult {
     private final URI context = URI.create(Contexts.PUBLICATION_CHANNEL_CONTEXT);
     @JsonProperty(ID_FIELD)
     private final URI id;
+
+    @JsonProperty(IDENTIFIER_FIELD)
+    private final String identifier;
+
     @JsonProperty(NAME_FIELD)
     private final String name;
     @JsonProperty(ONLINE_ISSN_FIELD)
@@ -41,12 +47,14 @@ public final class SeriesResult {
 
     @JsonCreator
     private SeriesResult(@JsonProperty(ID_FIELD) URI id,
+                         @JsonProperty(IDENTIFIER_FIELD) String identifier,
                          @JsonProperty(NAME_FIELD) String name,
                          @JsonProperty(ONLINE_ISSN_FIELD) String onlineIssn,
                          @JsonProperty(PRINT_ISSN_FIELD) String printIssn,
                          @JsonProperty(SCIENTIFIC_VALUE_FIELD) ScientificValue scientificValue,
                          @JsonProperty(SAME_AS_FIELD) URI sameAs) {
         this.id = id;
+        this.identifier = identifier;
         this.name = name;
         this.onlineIssn = onlineIssn;
         this.printIssn = printIssn;
@@ -60,6 +68,7 @@ public final class SeriesResult {
                      .addChild(series.getIdentifier(), year)
                      .getUri();
         return new SeriesResult(id,
+                                series.getIdentifier(),
                                 series.getName(),
                                 series.getOnlineIssn(),
                                 series.getPrintIssn(),
@@ -77,6 +86,10 @@ public final class SeriesResult {
 
     public URI getId() {
         return id;
+    }
+
+    public String getIdentifier() {
+        return identifier;
     }
 
     public String getName() {
@@ -99,21 +112,14 @@ public final class SeriesResult {
         return sameAs;
     }
 
-    @JacocoGenerated
     @Override
+    @JacocoGenerated
     public int hashCode() {
-        return Objects.hash(getType(),
-                            getContext(),
-                            getId(),
-                            getName(),
-                            getOnlineIssn(),
-                            getPrintIssn(),
-                            getScientificValue(),
-                            getSameAs());
+        return Objects.hash(context, id, identifier, name, onlineIssn, printIssn, scientificValue, sameAs);
     }
 
-    @JacocoGenerated
     @Override
+    @JacocoGenerated
     public boolean equals(Object o) {
         if (this == o) {
             return true;
@@ -122,28 +128,19 @@ public final class SeriesResult {
             return false;
         }
         SeriesResult that = (SeriesResult) o;
-        return Objects.equals(getType(), that.getType())
-               && Objects.equals(getContext(), that.getContext())
-               && Objects.equals(getId(), that.getId())
-               && Objects.equals(getName(), that.getName())
-               && Objects.equals(getOnlineIssn(), that.getOnlineIssn())
-               && Objects.equals(getPrintIssn(), that.getPrintIssn())
-               && Objects.equals(getScientificValue(), that.getScientificValue())
-               && Objects.equals(getSameAs(), that.getSameAs());
+        return Objects.equals(context, that.context)
+               && Objects.equals(id, that.id)
+               && Objects.equals(identifier, that.identifier)
+               && Objects.equals(name, that.name)
+               && Objects.equals(onlineIssn, that.onlineIssn)
+               && Objects.equals(printIssn, that.printIssn)
+               && scientificValue == that.scientificValue
+               && Objects.equals(sameAs, that.sameAs);
     }
 
-    @JacocoGenerated
     @Override
+    @JacocoGenerated
     public String toString() {
-        return "PublisherResult{"
-               + "type='" + type + '\''
-               + ", context=" + context
-               + ", id=" + id
-               + ", name='" + name + '\''
-               + ", onlineIssn='" + onlineIssn + '\''
-               + ", printIssn='" + printIssn + '\''
-               + ", scientificValue='" + scientificValue + '\''
-               + ", sameAs=" + sameAs
-               + '}';
+        return toJsonString();
     }
 }
