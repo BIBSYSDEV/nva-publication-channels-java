5138bcec9db13d1839922288ee5ff6ac28e2debb
monaullah <monaullah98@gmail.com>
2024-10-30 12:16:10 +0100

    Fix test setup for search

--- src/test/java/no/sikt/nva/pubchannels/handler/TestData.java
+++ src/test/java/no/sikt/nva/pubchannels/handler/TestData.java
@@ -4,7 +4,9 @@ import static no.sikt.nva.pubchannels.handler.TestUtils.scientificValueToLevel;
 import static no.unit.nva.commons.json.JsonUtils.dtoObjectMapper;
 import static no.unit.nva.testutils.RandomDataGenerator.randomElement;
 import static no.unit.nva.testutils.RandomDataGenerator.randomInteger;
+import static no.unit.nva.testutils.RandomDataGenerator.randomIssn;
 import static no.unit.nva.testutils.RandomDataGenerator.randomString;
+import static no.unit.nva.testutils.RandomDataGenerator.randomUri;
 import static nva.commons.core.attempt.Try.attempt;
 import java.net.URI;
 import no.sikt.nva.pubchannels.channelregistry.model.ChannelRegistryJournal;
@@ -16,15 +18,15 @@ public class TestData {
 
     private final String identifier;
     private final Integer year;
-    private final String name;
-    private final String originalTitle;
-    private final String onlineIssn;
-    private final String printIssn;
-    private final String isbnPrefix;
-    private final ScientificValue scientificValue;
-    private final String discontinued;
-    private final String sameAs;
-    private final Integer totalResults;
+    private String name;
+    private String originalTitle;
+    private String onlineIssn;
+    private String printIssn;
+    private String isbnPrefix;
+    private ScientificValue scientificValue;
+    private String discontinued;
+    private String sameAs;
+    private Integer totalResults;
 
     private TestData(Integer year, String identifier, String name,
                      String originalTitle, String onlineIssn, String printIssn, String isbnPrefix,
@@ -47,6 +49,21 @@ public class TestData {
     public TestData(Integer year, String identifier) {
         this(year, identifier, randomString(), randomString(), randomString(), randomString(), randomString(),
              randomElement(ScientificValue.values()), randomString(), randomString(), randomInteger());
+    }
+
+    public TestData withName(String name){
+        this.name = name;
+        return this;
+    }
+
+    public TestData withOnlineIssn(String onlineIssn) {
+        this.onlineIssn = onlineIssn;
+        return this;
+    }
+
+    public TestData withPrintIssn(String printIssn) {
+        this.printIssn = printIssn;
+        return this;
     }
 
     public JournalDto asJournalDto(URI selfUriBase, String requestedYear) {
