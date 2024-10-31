e3a3b3ba37c9050e91305024bc3b8da362fa6333
monaullah <monaullah98@gmail.com>
2024-10-31 13:08:40 +0100

    Refactor json serialization

--- src/test/java/no/sikt/nva/pubchannels/handler/TestChannel.java
+++ src/test/java/no/sikt/nva/pubchannels/handler/TestChannel.java
@@ -1,12 +1,10 @@
 package no.sikt.nva.pubchannels.handler;
 
 import static no.sikt.nva.pubchannels.handler.TestUtils.scientificValueToLevel;
-import static no.unit.nva.commons.json.JsonUtils.dtoObjectMapper;
 import static no.unit.nva.testutils.RandomDataGenerator.randomElement;
 import static no.unit.nva.testutils.RandomDataGenerator.randomIssn;
 import static no.unit.nva.testutils.RandomDataGenerator.randomString;
 import static no.unit.nva.testutils.RandomDataGenerator.randomUri;
-import static nva.commons.core.attempt.Try.attempt;
 import java.net.URI;
 import no.sikt.nva.pubchannels.channelregistry.model.ChannelRegistryJournal;
 import no.sikt.nva.pubchannels.channelregistry.model.ChannelRegistryLevel;
@@ -43,51 +41,57 @@ public record TestChannel(String identifier,
     }
 
     public String asChannelRegistryJournalBody() {
-        var channelRegistryBody = new ChannelRegistryJournal(identifier, name, onlineIssn.value(), printIssn.value(),
-                                                             new ChannelRegistryLevel(year, scientificValueToLevel(
-                                                                 scientificValue)),
-                                                             sameAs,
-                                                             discontinued);
-        return attempt(() -> dtoObjectMapper.writeValueAsString(channelRegistryBody)).orElseThrow();
+        return new ChannelRegistryJournal(identifier,
+                                          name,
+                                          onlineIssn.value(),
+                                          printIssn.value(),
+                                          new ChannelRegistryLevel(year, scientificValueToLevel(scientificValue)),
+                                          sameAs,
+                                          discontinued)
+                   .toJsonString();
     }
 
     public String asChannelRegistrySeriesBody() {
-        var channelRegistryBody = new ChannelRegistrySeries(identifier, name, onlineIssn.value(), printIssn.value(),
-                                                            new ChannelRegistryLevel(year, scientificValueToLevel(
-                                                                scientificValue)),
-                                                            sameAs,
-                                                            discontinued);
-        return attempt(() -> dtoObjectMapper.writeValueAsString(channelRegistryBody)).orElseThrow();
+        return new ChannelRegistrySeries(identifier,
+                                         name,
+                                         onlineIssn.value(),
+                                         printIssn.value(),
+                                         new ChannelRegistryLevel(year, scientificValueToLevel(scientificValue)),
+                                         sameAs,
+                                         discontinued)
+                   .toJsonString();
     }
 
     public String asChannelRegistryPublisherBody() {
-        var channelRegistryBody = new ChannelRegistryPublisher(identifier,
-                                                               new ChannelRegistryLevel(year, scientificValueToLevel(
-                                                                   scientificValue)), isbnPrefix.value(), name, sameAs,
-                                                               discontinued);
-        return attempt(() -> dtoObjectMapper.writeValueAsString(channelRegistryBody)).orElseThrow();
+        return new ChannelRegistryPublisher(identifier,
+                                            new ChannelRegistryLevel(year, scientificValueToLevel(scientificValue)),
+                                            isbnPrefix.value(),
+                                            name,
+                                            sameAs,
+                                            discontinued)
+                   .toJsonString();
     }
 
     public JournalDto asJournalDto(URI selfUriBase, String requestedYear) {
-        var expectedId = generateExpectedId(selfUriBase, requestedYear);
-        return new JournalDto(expectedId, identifier, name, onlineIssn.value(), printIssn.value(),
+        var id = generateIdWithYear(selfUriBase, requestedYear);
+        return new JournalDto(id, identifier, name, onlineIssn.value(), printIssn.value(),
                               scientificValue, sameAs, discontinued, requestedYear);
     }
 
     public SeriesDto asSeriesDto(String selfUriBase, String requestedYear) {
-        var expectedId = generateExpectedId(URI.create(selfUriBase), requestedYear);
-        return new SeriesDto(expectedId, identifier, name, onlineIssn.value(), printIssn.value(), scientificValue,
+        var id = generateIdWithYear(URI.create(selfUriBase), requestedYear);
+        return new SeriesDto(id, identifier, name, onlineIssn.value(), printIssn.value(), scientificValue,
                              sameAs,
                              discontinued, requestedYear);
     }
 
     public PublisherDto asPublisherDto(String selfUriBase, String requestedYear) {
-        var expectedId = generateExpectedId(URI.create(selfUriBase), requestedYear);
-        return new PublisherDto(expectedId, identifier, name, isbnPrefix.value(), scientificValue, sameAs, discontinued,
+        var id = generateIdWithYear(URI.create(selfUriBase), requestedYear);
+        return new PublisherDto(id, identifier, name, isbnPrefix.value(), scientificValue, sameAs, discontinued,
                                 requestedYear);
     }
 
-    private URI generateExpectedId(URI selfUriBase, String requestedYear) {
+    private URI generateIdWithYear(URI selfUriBase, String requestedYear) {
         return UriWrapper.fromUri(selfUriBase).addChild(identifier, requestedYear).getUri();
     }
 
