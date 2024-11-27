86706971bfa7affa46abef50d6d31433eee8fa48
monaullah <monaullah98@gmail.com>
2024-11-27 14:18:00 +0100

    Code cleanup

--- src/main/java/no/sikt/nva/pubchannels/handler/PublicationChannelSearchCreateClient.java
+++ src/main/java/no/sikt/nva/pubchannels/handler/PublicationChannelSearchCreateClient.java
@@ -2,10 +2,9 @@ package no.sikt.nva.pubchannels.handler;
 
 import java.util.Map;
 import no.sikt.nva.pubchannels.channelregistry.ChannelType;
+import no.sikt.nva.pubchannels.channelregistry.model.create.ChannelRegistryCreatePublisherRequest;
 import no.sikt.nva.pubchannels.channelregistry.model.create.ChannelRegistryCreateSerialPublicationRequest;
 import no.sikt.nva.pubchannels.channelregistry.model.create.CreateChannelResponse;
-import no.sikt.nva.pubchannels.channelregistry.model.create.ChannelRegistryCreatePublisherRequest;
-import no.sikt.nva.pubchannels.channelregistry.model.create.ChannelRegistryCreateSeriesRequest;
 import no.sikt.nva.pubchannels.handler.search.ThirdPartySearchResponse;
 import nva.commons.apigateway.exceptions.ApiGatewayException;
 
@@ -14,10 +13,12 @@ public interface PublicationChannelSearchCreateClient {
     ThirdPartySearchResponse searchChannel(ChannelType type, Map<String, String> queryParameters)
         throws ApiGatewayException;
 
-    CreateChannelResponse createJournal(ChannelRegistryCreateSerialPublicationRequest request) throws ApiGatewayException;
+    CreateChannelResponse createJournal(ChannelRegistryCreateSerialPublicationRequest request)
+        throws ApiGatewayException;
 
     CreateChannelResponse createPublisher(ChannelRegistryCreatePublisherRequest request)
         throws ApiGatewayException;
 
-    CreateChannelResponse createSeries(ChannelRegistryCreateSerialPublicationRequest request) throws ApiGatewayException;
+    CreateChannelResponse createSeries(ChannelRegistryCreateSerialPublicationRequest request)
+        throws ApiGatewayException;
 }
