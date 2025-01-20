71387c8cb36e866a2a59145fd9786832aac9ab11
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2025-01-20 14:28:05 +0100

    style: Reformat with Spotless (again)

--- src/main/java/no/sikt/nva/pubchannels/channelregistry/mapper/ScientificValueMapper.java
+++ src/main/java/no/sikt/nva/pubchannels/channelregistry/mapper/ScientificValueMapper.java
@@ -10,10 +10,10 @@ import no.sikt.nva.pubchannels.handler.ScientificValue;
 
 public class ScientificValueMapper {
 
-    public static final Map<String, ScientificValue> VALUES =
-        Map.of("0", LEVEL_ZERO, "1", LEVEL_ONE, "2", LEVEL_TWO);
+  public static final Map<String, ScientificValue> VALUES =
+      Map.of("0", LEVEL_ZERO, "1", LEVEL_ONE, "2", LEVEL_TWO);
 
-    public ScientificValue map(String value) {
-        return nonNull(value) ? VALUES.get(value) : UNASSIGNED;
-    }
+  public ScientificValue map(String value) {
+    return nonNull(value) ? VALUES.get(value) : UNASSIGNED;
+  }
 }
