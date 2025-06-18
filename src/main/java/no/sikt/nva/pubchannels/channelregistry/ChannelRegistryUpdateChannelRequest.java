9c609235e8bca0229822087a426a3dd41e50b846
Kir Truhacev <kir.truhacev@sikt.no>
2025-06-18 15:10:13 +0200

    ignoring type on serialization

--- src/main/java/no/sikt/nva/pubchannels/channelregistry/ChannelRegistryUpdateChannelRequest.java
+++ src/main/java/no/sikt/nva/pubchannels/channelregistry/ChannelRegistryUpdateChannelRequest.java
@@ -1,8 +1,9 @@
 package no.sikt.nva.pubchannels.channelregistry;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.UUID;
 import no.unit.nva.commons.json.JsonSerializable;
 
 public record ChannelRegistryUpdateChannelRequest(
-    UUID pid, String name, String pissn, String eissn, String isbnPrefix, String type)
+    UUID pid, String name, String pissn, String eissn, String isbnPrefix, @JsonIgnore String type)
     implements JsonSerializable {}
