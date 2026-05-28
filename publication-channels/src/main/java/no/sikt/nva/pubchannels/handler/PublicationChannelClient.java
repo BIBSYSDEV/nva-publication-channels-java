7500bca2fd75e00cd5cd9fcce35f52fcfabd5c75
Torbjørn Kvåle <torbjorn.kvale@sikt.no>
2026-05-28 21:51:33 +0200

    feat(handler): add backend-only delete endpoint for publication channels #NP-51227

--- publication-channels/src/main/java/no/sikt/nva/pubchannels/handler/PublicationChannelClient.java
+++ publication-channels/src/main/java/no/sikt/nva/pubchannels/handler/PublicationChannelClient.java
@@ -3,4 +3,5 @@ package no.sikt.nva.pubchannels.handler;
 public interface PublicationChannelClient
     extends PublicationChannelSearchCreateClient,
         PublicationChannelFetchClient,
-        PublicationChannelUpdateClient {}
+        PublicationChannelUpdateClient,
+        PublicationChannelDeleteClient {}
